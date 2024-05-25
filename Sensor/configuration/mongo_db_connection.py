d3c09d214046d3e71e13bd1d56c658513514b961
Gaurav <gkumardav1996@gmail.com>
2024-05-25 17:54:55 +0530

    Updated the mongodb or atlas password

--- Sensor/configuration/mongo_db_connection.py
+++ Sensor/configuration/mongo_db_connection.py
@@ -8,7 +8,7 @@ class  MongoDBClient:
     def __init__(self,database_name = DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://gkumardav:<password>@cluster.3kmskua.mongodb.net/?retryWrites=true&w=majority&appName=CLUSTER"
+                mongo_db_url = "mongodb+srv://gkumardav:Gaurav1993@cluster.3kmskua.mongodb.net/?retryWrites=true&w=majority&appName=CLUSTER"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tlsCAFile = ca)
             self.client = MongoDBClient.client
             self.database_name = database_name
