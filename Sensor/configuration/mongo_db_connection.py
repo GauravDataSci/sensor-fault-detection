1beae57330abd7c8c4b4e8ef4140d5c875ac898c
Gaurav <gkumardav1996@gmail.com>
2024-06-02 18:43:56 +0530

    This is the complete project

--- Sensor/configuration/mongo_db_connection.py
+++ Sensor/configuration/mongo_db_connection.py
@@ -1,17 +1,34 @@
-import pymongo
-from Sensor.constant.database import DATABASE_NAME
+import os
+import sys
+
 import certifi
+import pymongo
+
+from Sensor.constant.database import DATABASE_NAME
+from Sensor.constant.env_variable import MONGODB_URL_KEY
+from Sensor.exception import SensorException
+
 ca = certifi.where()
 
-class  MongoDBClient:
+
+class MongoDBClient:
     client = None
-    def __init__(self,database_name = DATABASE_NAME) -> None:
+
+    def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://gkumardav:Gaurav1993@cluster.3kmskua.mongodb.net/?retryWrites=true&w=majority&appName=CLUSTER"
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tlsCAFile = ca)
-            self.client = MongoDBClient.client
-            self.database = self.client[database_name]
-        except Exception as e:
-            raise e        
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
 
+                if mongo_db_url is None:
+                    raise Exception(f"Environment key: {MONGODB_URL_KEY} is not set.")
+
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+
+            self.client = MongoDBClient.client
+
+            self.database = self.client[database_name]
+
+            self.database_name = database_name
+
+        except Exception as e:
+            raise SensorException(e, sys)
